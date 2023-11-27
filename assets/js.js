7b5cb6ebde4a62200073cafe2b8ce9cdc2f19ef8
JMarques12 <beckham.souza06@eaportal.org>
2023-11-27 10:48:40 -0300

    Portfolio

--- assets/js.js
+++ assets/js.js
@@ -1,25 +1,3 @@
-// // alert("Bem Vindo ao meu Portfolio");
-
-// function setCookie(nome, valor, dias){ 
-// diasms = (new Date()) .getTime() + 1000 * 3600 * 24 * dias; 
-// dias = new Date(diasms); 
-// expires = dias.toGMTString(); 
-// document.cookie = escape(nome) + "=" + escape(valor) + "; expires=" + expires; 
-// } 
-
-// if (!document.cookie){
-//     setCookie("cookie", "1", 365);
-//     document.write("<font face='verdana' size='1'>Suas Visitas : 1</font>");
-//     } else {
-//     var cont = document.cookie;
-//     var dividindo = cont.split("=");
-//     //document.write(dividindo[1]);
-//     var numero = parseInt(dividindo[1]);
-//     var soma = numero + 1;
-//     document.write("<font face='verdana' size='1'>Suas Visitas : " + soma + "</font>");
-//     setCookie("cookie", soma, 365);
-//     }
-
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
       this.mobileMenu = document.querySelector(mobileMenu);
@@ -39,7 +17,7 @@ class MobileNavbar {
             }s`);
       });
     }
-  
+
     handleClick() {
       this.navList.classList.toggle(this.activeClass);
       this.mobileMenu.classList.toggle(this.activeClass);
@@ -48,6 +26,10 @@ class MobileNavbar {
   
     addClickEvent() {
       this.mobileMenu.addEventListener("click", this.handleClick);
+      
+      this.navLinks.forEach(e => {
+        e.addEventListener("click", this.handleClick)
+      })
     }
   
     init() {
@@ -61,6 +43,6 @@ class MobileNavbar {
   const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
-    ".nav-list li",
+    ".nav-list a",
   );
   mobileNavbar.init();
