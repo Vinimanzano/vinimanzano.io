4500f6af0a4723c49bc704ad2f5b48ee97fd58e0
vinimanzano <vini.manzano5@gmail.com>
2023-12-04 15:36:16 -0300

    SENAI

--- assets/js.js
+++ assets/js.js
@@ -76,4 +76,27 @@ if (!document.cookie){
 //     e.preventDefault(); })
 // console.log
 
+let percent = 0
+let time          
 
+function animate(id1, id2, start, end, duration) {
+  if (start === end) return;
+  const range = end - start;
+  let current = start;
+  const increment = end > start ? 1 : -1;
+  const stepTime = Math.abs(Math.floor(duration / range));
+  const obj1 = document.getElementById(id1);
+  const obj2 = document.getElementById(id2);
+  const timer = setInterval(function () {
+    current += increment;
+    obj1.innerHTML = current;
+    obj2.style.strokeDashoffset = `${440 - (440 * current) / 100}`
+    if (current === end) {
+      clearInterval(timer);
+    }
+  }, stepTime);
+}
+
+animate("percent1", "circle1", 0, 40, 5000)
+animate("percent2", "circle2", 0, 30, 5000)
+animate("percent3", "circle3", 0, 30, 5000)
